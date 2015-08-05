4990c79c4fd1d1d0dc041e567b9587ed1d17f33b
Ben Westrate <benjamin.westrate@icfi.com>
2015-08-05 10:12:38 -0500

    compiled the es2015 to es5

--- index.js
+++ index.js
@@ -1,118 +1,136 @@
-module.exports = class Stoar {
+'use strict';
 
-    constructor(){
+var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
+
+module.exports = (function () {
+    function Stoar() {
+        _classCallCheck(this, Stoar);
 
         this.AllData = [];
 
-        var data          = this.getAllData();
-        var componentList = this._list( data, 'componentName' );
-        var components    = [];
-        var self          = this;
+        var data = this.getAllData();
+        var componentList = this._list(data, 'componentName');
+        var components = [];
+        var self = this;
 
-        componentList.map( function( component ){
+        componentList.map(function (component) {
             components.push({
-                name : component, 
-                instances : self.getInstances( component )
+                name: component,
+                instances: self.getInstances(component)
             });
         });
 
         return {
-            components
+            components: components
         };
     }
 
     /*
      * Filter an array by a passed key and value
      */
-    _filter( arr, key, value ){
-        return arr.filter( function( i ){
-            return i[ key ] === value;
-        });
-    }
 
-    /*
-     * Returns a list matching the value passed of the
-     */
-    _list( arr, value ){
+    _createClass(Stoar, [{
+        key: '_filter',
+        value: function _filter(arr, key, value) {
+            return arr.filter(function (i) {
+                return i[key] === value;
+            });
+        }
 
-        var list = [];
-        arr.map( function( inst ){
+        /*
+         * Returns a list matching the value passed of the
+         */
+    }, {
+        key: '_list',
+        value: function _list(arr, value) {
 
-            var key = inst[ value ] || 0;
-            if ( list.indexOf( key ) < 0 ){
-                list.push( key );
-            }
-        });
+            var list = [];
+            arr.map(function (inst) {
 
-        return list;
-    }
+                var key = inst[value] || 0;
+                if (list.indexOf(key) < 0) {
+                    list.push(key);
+                }
+            });
 
-    _cleanup( data ){
-        return data.filter( function( item ){ 
-            delete item.componentConfig;
-            delete item.componentInstance;
-            delete item.componentName
-            return item;
-        });
-    }
+            return list;
+        }
+    }, {
+        key: '_cleanup',
+        value: function _cleanup(data) {
+            return data.filter(function (item) {
+                delete item.componentConfig;
+                delete item.componentInstance;
+                delete item.componentName;
+                return item;
+            });
+        }
 
-    /*
-     * Returns array of instances of the passed component name
-     */
-    getInstances( component ){
+        /*
+         * Returns array of instances of the passed component name
+         */
+    }, {
+        key: 'getInstances',
+        value: function getInstances(component) {
 
-        var allInstanceData = this._filter( this.AllData, 'componentName', component );
-        var instances       = this._list( allInstanceData, 'componentInstance' );
-        var self            = this;
-        
-        return instances.map( function( inst ){
+            var allInstanceData = this._filter(this.AllData, 'componentName', component);
+            var instances = this._list(allInstanceData, 'componentInstance');
+            var self = this;
 
-            var data   = self._filter( allInstanceData, 'componentInstance', inst );
-            var config = self._filter( data, 'componentConfig', true );
-                data   = self._cleanup( self._filter ( data, 'componentConfig', false ) );
+            return instances.map(function (inst) {
 
-            var obj = {
-                id   : inst,
-                data
-            };
+                var data = self._filter(allInstanceData, 'componentInstance', inst);
+                var config = self._filter(data, 'componentConfig', true);
+                data = self._cleanup(self._filter(data, 'componentConfig', false));
 
-            // Add the config if it exists
-            if ( config.length ){ obj.config = self._cleanup( config )[0]; }
+                var obj = {
+                    id: inst,
+                    data: data
+                };
 
-            return obj;        
-        });
-    }
+                // Add the config if it exists
+                if (config.length) {
+                    obj.config = self._cleanup(config)[0];
+                }
 
-    /*
-     * Scrapes the DOM for all script tags with the `data-component` attribute
-     */
-    getAllData(){
+                return obj;
+            });
+        }
 
-        // Grab the component script elements
-        var componentScripts = document.querySelectorAll('[data-component]');
+        /*
+         * Scrapes the DOM for all script tags with the `data-component` attribute
+         */
+    }, {
+        key: 'getAllData',
+        value: function getAllData() {
 
-        // Parse the scripts from components and add their data to the Store
-        for ( var i = 0; i < componentScripts.length; i++ ){
+            // Grab the component script elements
+            var componentScripts = document.querySelectorAll('[data-component]');
 
-            // Localize variables
-            var script = componentScripts[i],
-                name   = script.dataset.component,
-                config = script.dataset.componentConfig !== undefined,
-                componentInstance = script.dataset.componentInstance,
-                data   = JSON.parse( script.innerHTML );
+            // Parse the scripts from components and add their data to the Store
+            for (var i = 0; i < componentScripts.length; i++) {
+
+                // Localize variables
+                var script = componentScripts[i],
+                    name = script.dataset.component,
+                    config = script.dataset.componentConfig !== undefined,
+                    componentInstance = script.dataset.componentInstance,
+                    data = JSON.parse(script.innerHTML);
 
                 // Add to the data JSON object
                 data.componentName = name;
                 data.componentInstance = componentInstance;
                 data.componentConfig = config;
 
-            // Add the data to the store, and initialize the app
-            this.AllData.push( data );
+                // Add the data to the store, and initialize the app
+                this.AllData.push(data);
+            }
 
+            return this.AllData;
         }
+    }]);
 
-        return this.AllData;
-
-    }
-
-};
+    return Stoar;
+})();
